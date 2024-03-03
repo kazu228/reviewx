0207547ba226ce973374597f648ceb25dd7bf05b
Kazu <kskkm0228@gmail.com>
2024-03-03 17:55:10 +0900

    文言の修正

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -6,11 +6,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1 class="mb-1"><a href="{{ route('home') }}" style="text-decoration:none; color:inherit;">{{ config('app.name') }}</a></h1>
-            <h3>タイトルのレビュー</h3>
             
             <div class="card">
                 <div class="card-header">
-                    <h4>ID:12345 ○○○○さんのレビュー依頼</h4>
+                    <h4>ID:{{ $review->user_id }} {{ $review->user_name }}さんのレビュー依頼</h4>
                     <h4>{{ $review->title }}</h4>
                 </div>
 
