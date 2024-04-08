980a5e0d7244e45e8ccbb9b7e0ea61c7739180fc
Kazu <kskkm0228@gmail.com>
2024-04-08 21:16:54 +0900

    複数ファイルアップロードのため修正

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -42,7 +42,7 @@
 
                             <div class="col-md-6">
                                 {{-- <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"> --}}
-                                <input type="file" name="contents[]" multiple />
+                                <input type="file" name="contents[]"  multiple="multiple" />
                             </div>
                         </div>
 
